28611459cb5dfcaa46f73c9643e4cc44e7da5e64
Luiz Becker <luizhenriquebecker000@gmail.com>
2025-05-07 20:33:22 -0300

    chose: interface

--- ListaDeExerciciosPOO/ClasseContaBancaria/main.py
+++ ListaDeExerciciosPOO/ClasseContaBancaria/main.py
@@ -3,10 +3,19 @@ import os
 
 os.system('cls' if os.name=='nt' else 'clear') 
 
-orlando = contaBancaria("Orlando", 1000, 500, []) # conta1
-orlando.depositar(50)
-henrique = contaBancaria("Henrique", 1000, 500, []) # conta2
+# orlando = contaBancaria("Orlando", 1000, 500, []) # conta1
+# orlando.depositar(50)
+# henrique = contaBancaria("Henrique", 1000, 500, []) # conta2
 
-henrique.transferir(50, orlando)
-henrique.exibir_historico()
-orlando.exibir_historico()
+# henrique.transferir(50, orlando)
+# henrique.exibir_historico()
+# orlando.exibir_historico()
+
+menu = "Selecione a operação desejada!"
+menu += " 1 - Criar conta;",
+menu += " 2 - Exibir saldo;",
+menu += " 3 - Sacar;",
+menu += " 4 - Depositar;",
+menu += " 5 - "
+menu += " 6 - "
+menu += " 7 - "
