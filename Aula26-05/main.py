066750ee38da0b8245e32fd858a03cdf6e0320b6
Luiz Becker <luizhenriquebecker000@gmail.com>
2025-05-26 21:44:51 -0300

    ...

--- Aula26-05/main.py
+++ Aula26-05/main.py
@@ -1,7 +1,17 @@
 import tkinter as tk
+def clique():
+    if rotulo.cget("text") == "Olá Mundo":
+        rotulo.config(text="Você clicou no botão!")
+    else:
+        rotulo.config(text="Olá Mundo")
 
-# Este é um exemplo básico de uma janela Tkinter.
 janela = tk.Tk()
-janela.title("Aula 26 - Tkinter")
-janela.geometry("400x300")
+janela.geometry("600x400")
+
+rotulo = tk.Label(janela, text="Clique no botão abaixo:", font=("Arial", 16))
+rotulo.pack(pady=10)
+
+botao = tk.Button(janela, text="Clique aqui", command=clique)
+botao.pack(pady=10)
+
 janela.mainloop()
